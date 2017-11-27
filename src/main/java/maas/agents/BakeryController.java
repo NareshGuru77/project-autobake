f3cc231d356d784ee7b3ed4546896e5dc6f39fb9
Debaraj Barua <debaraj.barua@smail.inf.h-brs.de>
2017-11-27 02:31:42 +0100

    updated agents to enable communication

--- src/main/java/maas/agents/BakeryController.java
+++ src/main/java/maas/agents/BakeryController.java
@@ -1,6 +1,10 @@
 package maas.agents;
 
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
+import maas.models.Order;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,9 +27,33 @@ public class BakeryController extends Agent {
 	
 	protected void setup() {
 		System.out.println("Hello! BakeryController-agent " + getAID().getName() + " is ready.");
+		addBehaviour(new OrderHandleServer());
 		
 	}
 	protected void takeDown() {
 		System.out.println("BakeryController-agent " + getAID().getName() + " terminated.");
 	}
+	
+	private class OrderHandleServer extends CyclicBehaviour {
+		public void action() {
+			ACLMessage msg = myAgent.receive();
+			
+			if (msg != null) {
+				try {
+					Order new_order = ((Order)msg.getContentObject());
+					System.out.println("Received Order id: "+new_order.getGuid()+" from Customer: "+new_order.getCustomerId());
+					System.out.println("\nOrder Details:\n_______________________\n");
+					System.out.println("Order Date: "+ new_order.getOrderDate()+ " Delivery Date: "+new_order.getDeliveryDate());
+				} catch (UnreadableException e) {
+					e.printStackTrace();
+				}
+				
+			} else {
+				block();
+
+}
+			
+		}
+	}  // End of inner class OfferRequestsServer
+	
 }
--- src/main/java/maas/agents/Customer.java
+++ src/main/java/maas/agents/Customer.java
@@ -2,10 +2,14 @@ package maas.agents;
 
 import jade.core.Agent;
 import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
 
+import java.io.IOException;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import maas.models.Date;
 import maas.models.Location;
 import maas.models.Order;
 import maas.models.Product;
@@ -24,7 +28,7 @@ public class Customer extends Agent {
 	protected void setup() {
 		System.out.println("Hello! Customer-agent " + getAID().getName() + " is ready.");
 		//System.out.println("Customer guid: "+guid+ "; Customer location is: ("+location.getX()+","+location.getY()+")");
-		 addBehaviour(new orderProducts());
+		 addBehaviour(new orderProducts(orders.get(0)));
 	}
 
 	protected void takeDown() {
@@ -32,9 +36,25 @@ public class Customer extends Agent {
 	}
 
 	private class orderProducts extends OneShotBehaviour {
+		Order new_order;
+		public orderProducts(Order order) {
+			Date check_date=new Date();
+			new_order=new Order("guid_check", "customerId_check", check_date,check_date);
+		}
+
 		public void action() {
 			System.out.println("\nNumber of orders from customer"+ getAID().getName() +" is: "+orders.size());
-			for (Order order :orders){
+			jade.lang.acl.ACLMessage aclmsg = new ACLMessage(ACLMessage.REQUEST);
+			
+			
+			try {
+				aclmsg.setContentObject((Serializable) new_order);
+				send(aclmsg);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	        
+			/*for (Order order :orders){
 				System.out.println("\nOrder id:"+order.getGuid());
 				System.out.println("\nOrder Date:"+order.getOrderDate().toString()+"; "
 						+ "Delivery Date: "+order.getDeliveryDate().toString());
@@ -46,7 +66,8 @@ public class Customer extends Agent {
 					System.out.println("\n"+count+") Item:"+productsToOrder.getProductid()+
 							"; Quantity: "+productsToOrder.getQuantity());					
 				}
-			}
+			}*/
+			
 		}
 	}
 
