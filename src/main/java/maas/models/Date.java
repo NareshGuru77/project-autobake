1d0e23cd0d8c9dba86dfd814deab34dce76d4f44
Ramit Sharma <you@example.com>
2017-12-01 11:56:00 +0100

    made changes to date for writing test case

--- src/main/java/maas/models/Date.java
+++ src/main/java/maas/models/Date.java
@@ -2,8 +2,16 @@ package maas.models;
 
 public class Date {
 	
+	
 	int day;
 	int hour;
+	
+	public Date(int day2, int hour2) {
+		// TODO Auto-generated constructor stub
+		day = day2;
+		hour = hour2;		
+		System.out.println(day2);
+	}
 	public int getDay() {
 		return day;
 	}
